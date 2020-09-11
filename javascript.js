1378984c2e5dd8d08ad6d2963c97b79fcedfa8e8
ver Pasamic <vpasamic22@gmail.com>
2020-09-11 16:59:33 -0700

    consolidated random variables

--- javascript.js
+++ javascript.js
@@ -1,19 +1,32 @@
-var a = "abcdefghijklmnopqrstuvwxyz";
-var b = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var c ="!#$%&'()*+,-./:;<=>?@[\]^_`{|}~" ;
-var d = "1234567890";
-var lwrcase = a.split("");
-var uprcase = b.split("");
-var nmbs = d.split("");
-var spec = c.split("");
+var a = "<=>?@!#$%&abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
 
-console.log(a);
-console.log(b);
-console.log(c);
-console.log(d);
-console.log(lwrcase);
-console.log(uprcase);
-console.log(nmbs);
+var character = a.split("");
+
+
+
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+function randomtype(){
+    console.log(character[Math.floor(Math.random()*72)])
+};
+
+
+
+randomtype()
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
 
 
 // function passgen(event){
