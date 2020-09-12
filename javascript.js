abd86cc59a76e8bb5fba49960914652e4a444c40
Ver <vpasamic22@gmail.com>
2020-09-11 22:56:53 -0700

    changed if statement to include what happens if its not a number

--- javascript.js
+++ javascript.js
@@ -15,16 +15,14 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-    var length = parseInt(prompt("How long of a password?"));
+    var lngth = parseInt(prompt("How long of a password?"));
     //make it between 8 to 128
-    if(length<8 || length>128){
-        alert("password must be between 8 to 128");
-    }
-    else{
+   
+    if(lngth>8 && lngth<128){
     var passwordText = document.querySelector("#password");
     function generatePassword(){
     //for loop length
-    for( var x=0; x < length; x++){
+    for( var x=0; x < lngth; x++){
         //random character generator
         function randomtype(){
             var red = character[Math.floor(Math.random()*72)];
@@ -34,10 +32,14 @@ function writePassword() {
     }
 }
 }
+    else{
+        alert("password must be a number between 8 to 128");
+    }
     generatePassword();
     passwordText.value=pswrd
 
 }
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
